8d77c6ab2950a6a70da4750bccbe2a4b5e522d78
Ayman Al-Shraiedeh <aymanshraideh96@gmail.com>
2022-06-11 14:57:38 +0300

    main

--- blood/routes/dashbordRoute.php
+++ blood/routes/dashbordRoute.php
@@ -1,9 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dashbord\adminIController;
+use Illuminate\Routing\Route as RoutingRoute;
 
-
-
-Route::get('/admin', function () {
-    return view('dashbord.indexDD');
+Route::get('/test', function () {
+    return view('dashbord.layout.header');
 });
+
+Route::get('/admin',[adminIController::class,'index']);
+Route::get('/admin',[adminIController::class,'show']);
+
+
+
+Route::get('/form', function () {
+    return view('dashbord.form');
+});
+
+Route::get('/element', function () {
+    return view('dashbord.element');
+});
+
+Route::get('/chart', function () {
+    return view('dashbord.chart');
+});
+
+Route::get('/button', function () {
+    return view('dashbord.button');
+});
+
