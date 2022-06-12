36e344d086ce41f8257bb035e542a5aeae6fd27f
mohamadamarneh <mohamadamarneh7@gmail.com>
2022-06-12 16:02:00 +0300

    any

--- blood/app/Http/Controllers/BloodDonerController.php
+++ blood/app/Http/Controllers/BloodDonerController.php
@@ -41,16 +41,16 @@ class BloodDonerController extends Controller
      */
     public function store(Storeblood_donerRequest $request)
     {
-        $input = Validator::make($request->all(),['b_d_name'=>'required|max:255' ,
-        'b_d_gender'=>'required|max:255' ,
-        'b_d_phone' =>'required|max:255' ,
-        'b_d_address'=>'required|max:255' ,
-        'b_d_email'=>'required|unique:blood_doners' ,
-        'b_d_password'=>'required|max:255' ,
-        'b_d_reprt'=>'required|max:255' ,
-        'b_d_age' =>'required|max:255',
-        'status' =>'required|max:255',
-        'b_d_blood_type'=>'required|max:255'])->validate();
+        // $input = Validator::make($request->all(),['b_d_name'=>'required|max:255' ,
+        // 'b_d_gender'=>'required|max:255' ,
+        // 'b_d_phone' =>'required|max:255' ,
+        // 'b_d_address'=>'required|max:255' ,
+        // 'b_d_email'=>'required|unique:blood_doners' ,
+        // 'b_d_password'=>'required|max:255' ,
+        // 'b_d_reprt'=>'required|max:255' ,
+        // 'b_d_age' =>'required|max:255',
+        // 'status' =>'required|max:255',
+        // 'b_d_blood_type'=>'required|max:255'])->validate();
 
         if($request->file('b_d_reprt')){
             $file= $request->file('b_d_reprt');
