b593d38ba46974f696894359093abff8938bfe27
Abood kh <abdalrhmankhashasneh@gmail.com>
2022-06-12 18:12:41 +0300

    edit doner and make doner login

--- blood/app/Http/Controllers/BloodDonerController.php
+++ blood/app/Http/Controllers/BloodDonerController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Updateblood_donerRequest;
 use App\Models\blood_type;
 use Illuminate\Http\Request;
 use Validator;
+use Session ; 
  use Illuminate\Support\Facades\Hash;
 class BloodDonerController extends Controller
 {
@@ -17,13 +18,25 @@ class BloodDonerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function login(Request $request){
+        if(Session::has('user_email'))
+        {
+            $user = blood_doner::where('b_d_email' , Session::get('user_email'))->first();
+            return view('blood_doner.profile' , compact('user'));
+        }
         return view('blood_doner.login');
     }
     public function check(Request $request){
+        
+        if($request->input('logout')){
+            $request->session()->flush();
+            return view('blood_doner.login'); 
+        }
+
         $user = blood_doner::where('b_d_email' , $request->input('Email'))->first();
         if(isset($user)){
             if(Hash::check($request->input('password'), $user->b_d_password)){
-                return view('blood_doner.profile');
+                $request->session()->put( 'user_email' , $user-> b_d_email );
+                return view('blood_doner.profile' , compact('user'));
             }
             else{
                 return 'email or password not correct ';
@@ -82,7 +95,7 @@ class BloodDonerController extends Controller
         }  
 
         blood_doner::create($input);    
-        return redirect('doner');
+        return redirect('login');
 }
 
 
@@ -95,7 +108,7 @@ class BloodDonerController extends Controller
      */
     public function show(blood_doner $blood_doner)
     {
-        //
+        
     }
 
     /**
@@ -104,9 +117,11 @@ class BloodDonerController extends Controller
      * @param  \App\Models\blood_doner  $blood_doner
      * @return \Illuminate\Http\Response
      */
-    public function edit(blood_doner $blood_doner)
+    public function edit()
     {
-        //
+        $user = blood_doner::where('b_d_email' , Session::get('user_email'))->first();
+        $blood = blood_type::all();
+        return view('blood_doner.update' , compact('user'))->with('blood' , $blood);
     }
 
     /**
@@ -118,7 +133,29 @@ class BloodDonerController extends Controller
      */
     public function update(Updateblood_donerRequest $request, blood_doner $blood_doner)
     {
-        //
+        $input = Validator::make($request->all(),['b_d_name'=>'required|max:255' ,
+        'b_d_gender'=>'required|max:255' ,
+        'b_d_phone' =>'required|max:255' ,
+        'b_d_address'=>'required|max:255' ,
+        'b_d_email'=>'required' ,
+        'b_d_password'=>'required|max:255' ,
+        'b_d_reprt'=>'required|max:255' ,
+        'b_d_age' =>'required|max:255',
+        'status' =>'required|max:255',
+        'b_d_blood_type'=>'required|max:255'])->validate();
+
+        $input['b_d_password'] = Hash::make($request->input('b_d_password'));
+
+        if($request->file('b_d_reprt')){
+            $file= $request->file('b_d_reprt');
+            $filename= date('YmdHi').$file->getClientOriginalName();
+            $file-> move(public_path('public/Image'), $filename);
+            $input['b_d_reprt'] = "$filename";
+        }  
+        $user = blood_doner::where('b_d_email' , Session::get('user_email'))->first();
+
+        blood_doner::where('b_d_email' ,Session::get('user_email') )->update($input); 
+        return redirect('login')->with('user' ,$user );
     }
 
     /**
